12540e77a7ac84fb51c85f84bebfd94c1753099f
lisaokezie <56400443+lisaokezie@users.noreply.github.com>
2020-07-29 22:43:11 +0200

    added authentication

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,23 @@
-@include('template.header')
+@extends('layouts.app')
+
+@section('content')
 <div class="container">
-        <!--        Headline-->
-        <div class="mx-auto px-3 py-3 pt-md-5 pb-md-4 text-center">
-            <h2 class="display-4">Rezepte</h2>
-            <p class="lead">Meine Rezeptsammlung</p>
-            <a href="/recipes/create" class="btn btn-outline-primary" role="button">Neues Rezept erstellen</a>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
         </div>
+    </div>
 </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,17 @@ use Illuminate\Http\Request;
 Route::redirect('/', '/recipes');
 
 Route::get('/recipes', 'RecipeController@index')->name('recipes.index');
-Route::get('/recipes/create', 'RecipeController@create');
+Route::get('/recipes/create', 'RecipeController@create')->middleware('auth');
 Route::post('/recipes', 'RecipeController@store');
 Route::get('/recipes/{recipe}', 'RecipeController@show');
-Route::get('/recipes/{recipe}/edit', 'RecipeController@edit');
-Route::patch('/recipes/{recipe}', 'RecipeController@update');
-Route::delete('/recipes/{recipe}', 'RecipeController@destroy');
+Route::get('/recipes/{recipe}/edit', 'RecipeController@edit')->middleware('auth');
+Route::patch('/recipes/{recipe}', 'RecipeController@update')->middleware('auth');
+Route::delete('/recipes/{recipe}', 'RecipeController@destroy')->middleware('auth');
 Route::any('/search','RecipeController@search');
 
 //Category
 Route::get('/categories', 'CategoryController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
