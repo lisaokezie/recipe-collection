8624bfa3d3a415e4679c8eaf498f833f12be5317
lisaokezie <56400443+lisaokezie@users.noreply.github.com>
2020-08-09 18:24:19 +0200

    minor fix

--- database/seeds/comments.sql
+++ database/seeds/comments.sql
@@ -7,11 +7,6 @@
 -- Server-Version: 10.4.11-MariaDB
 -- PHP-Version: 7.2.29
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
 INSERT INTO `comments` (`id`, `recipe_id`, `user_id`, `text`, `created_at`, `updated_at`) VALUES
 (1, 1, 2, 'Ich habe den Käsekuchen schon oft gebacken und bin begeistert von diesem Rezept!', '2020-08-07 21:52:23', '2020-08-07 21:52:23'),
 (2, 2, 2, 'Super Rezept! Ich mache es oft für Geburtstagsfeiern.', '2020-08-07 21:54:24', '2020-08-07 21:54:24'),
